4a8ff11344af8742219b11c385b76bcf3a7b00b0
matthieu jimenez <electricalwind@gmail.com>
2014-01-10 14:23:16 +0100

    p1 command line

--- Certificat/src/com/polytech/security/SqueletonmyKeystore.java
+++ Certificat/src/com/polytech/security/SqueletonmyKeystore.java
@@ -1,8 +1,11 @@
 package com.polytech.security;
 
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.TestGenerator;
+
 import java.security.*;
 import java.security.cert.*;
+import java.security.cert.Certificate;
 import java.security.interfaces.*;
 
 import java.io.*;
@@ -14,17 +17,46 @@ public class SqueletonmyKeystore{
 		try{
 		
 		// create a keystore with "JKS"
+        KeyStore ks = KeyStore.getInstance("JKS");
+
 		// load the keystore from the one created with Keytool tool
+            char[] password = "desstelecom".toCharArray();
 
+            java.io.FileInputStream fis = null;
+            try {
+                fis = new java.io.FileInputStream("/Users/matthieujimenez/Documents/APP/ApplicationSecurity_LabSession2/Certificat/src/com/polytech/security/commandline/myKeyStore.jks");
+                ks.load(fis, password);
+            } finally {
+                if (fis != null) {
+                    fis.close();
+                }
+            }
 		// checks if the key store contains your alias		
-		
+		boolean trouve=ks.containsAlias("matthieu");
 		// get the certificate associated to your alias
-		
-		// display it
+		if (trouve){
+            Certificate mat=ks.getCertificate("matthieu");
+            // display it
+            System.out.println(mat.toString());
+            // retreive the public key of the certificate
+            PublicKey pkmat=mat.getPublicKey();
+            // retreive the alias private key from the keystore
+            PrivateKey pk= (PrivateKey)ks.getKey("matthieu","matthieu".toCharArray());
+            Signature signingEngine = Signature.getInstance("DSA");
 
-		// retreive the public key of the certificate
-		
-		// retreive the alias private key from the keystore
+            SignedObject so = new SignedObject("ma signature",pk,
+                    signingEngine);
+            Signature verificationEngine =
+                    Signature.getInstance("DSA");
+            if (so.verify(pkmat, verificationEngine))
+                try {
+                    Object myobj = so.getObject();
+                    System.out.println(myobj);
+                } catch (java.lang.ClassNotFoundException e) {};
+        }
+
+
+
                  
 		// getInstance of a signObject 
 		// create a signedObject with 
